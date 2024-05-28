8abf097666b15138309c1e88962b696543eecf50
Javier Tinajo Gallardo <javiertinajogallardo@MBP-de-Javier-2.home>
2024-05-28 13:11:42 +0200

    Updated useAxios

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pokeapi.co/api/v2';
@@ -7,21 +7,25 @@ export const useAxios = (axiosParams) => {
     const [response, setResponse] = useState(undefined);
     const [error, setError] = useState({hasError: false});
     const [loading, setLoading] = useState(true);
+    const hasFetched = useRef(false); // Use ref to track if the request has already been made
 
     const fetchData = async (params) => {
-      try {
-        const result = await axios.request(params);
-        setResponse(result.data);
-      } catch( error ) {
-         setError({hasError: true, message: error.message});
-      } finally {
-        setLoading(false);
-      }
+        try {
+            const result = await axios.request(params);
+            setResponse(result.data);
+        } catch (error) {
+            setError({hasError: true, message: error.message});
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
-        fetchData(axiosParams);
-    }, []);
+        if (!hasFetched.current) { // Check if the request has already been made
+            fetchData(axiosParams);
+            hasFetched.current = true; // Mark as fetched
+        }
+    }, [axiosParams]); // Ensure axiosParams is included in the dependency array
     
     return { response, error, loading };
 };
